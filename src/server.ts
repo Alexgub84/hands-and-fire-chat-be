d0b704ac9f3ef129778d34838ac0b8df9253b297
Alex Guberman <alex.gub@gmail.com>
2025-10-27 16:46:54 +0200

    added husky and tests

--- src/server.ts
+++ src/server.ts
@@ -5,38 +5,46 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const app = fastify({ logger: true });
-await app.register(formbody);
+export async function buildApp() {
+  const app = fastify({ logger: process.env.NODE_ENV !== "test" });
+  await app.register(formbody);
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const waPhoneNumber = process.env.WA_PHONE_NUMBER;
-const client = twilio(accountSid, authToken);
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+  const authToken = process.env.TWILIO_AUTH_TOKEN;
+  const waPhoneNumber = process.env.WA_PHONE_NUMBER;
 
-app.get("/", (_req, reply) => {
-  reply.send({ ok: true });
-});
-
-app.post("/whatsapp", async (request, reply) => {
-  const { From, Body } = request.body as Record<string, string>;
-  app.log.info(`Received WhatsApp message from ${From}: ${Body}`);
-  if (!From || !Body) {
-    return reply.status(400).send({ error: "Missing required fields" });
-  }
-
-  await client.messages.create({
-    from: waPhoneNumber!,
-    to: From,
-    body: "SO cool!",
+  app.get("/", (_req, reply) => {
+    reply.send({ ok: true });
   });
-  reply.code(200).send({ ok: true });
-  return;
-});
 
-app.listen({ port: 3000 }, (err, address) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-  app.log.info(`Server is running on ${address}`);
-});
+  app.post("/whatsapp", async (request, reply) => {
+    const { From, Body } = request.body as Record<string, string>;
+    app.log.info(`Received WhatsApp message from ${From}: ${Body}`);
+    if (!From || !Body) {
+      return reply.status(400).send({ error: "Missing required fields" });
+    }
+
+    const client = twilio(accountSid, authToken);
+    await client.messages.create({
+      from: waPhoneNumber!,
+      to: From,
+      body: "SO cool!",
+    });
+    reply.code(200).send({ ok: true });
+    return;
+  });
+
+  return app;
+}
+
+if (process.env.NODE_ENV !== "test") {
+  const app = await buildApp();
+
+  app.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+    app.log.info(`Server is running on ${address}`);
+  });
+}
